06d4aeb4b26e0eef792dff42b7c99fe9cb9cb7e5
bartek <smalecbartlomiej@gmail.com>
2022-12-29 17:57:23 +0100

    adding POJOs, closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lastNamel;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNamel() {
+        return lastNamel;
+    }
+
+    public void setLastNamel(String lastNamel) {
+        this.lastNamel = lastNamel;
+    }
+}
