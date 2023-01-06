445e7dc13a9ab2c70afd3265ec1a95b6ee7b3d22
bartek <smalecbartlomiej@gmail.com>
2023-01-06 17:15:11 +0100

    Fixed findOwners() test. closes #64

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnersControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnersControllerTest.java
@@ -15,9 +15,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -43,29 +43,13 @@ class OwnersControllerTest {
                 .build();
     }
 
-//    @Test
-//    void listOwners() throws Exception {
-//        Mockito.when(ownerService.findAll()).thenReturn(ownerSet);
-//        mockMvc
-//                .perform(get("/owners"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("owners/index"))
-//                .andExpect(model().attribute("owners", hasSize(2)));
-//    }
-//    @Test
-//    void listOwnersByIndex() throws Exception {
-//        Mockito.when(ownerService.findAll()).thenReturn(ownerSet);
-//        mockMvc
-//                .perform(get("/owners/index"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("owners/index"))
-//                .andExpect(model().attribute("owners", hasSize(2)));
-//    }
-
     @Test
     void findOwners() throws Exception {
         mockMvc.perform(get("/owners/find"))
-                .andExpect(view().name("notimplemented"));
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(model().attributeExists("owner"));
+
         Mockito.verifyNoInteractions(ownerService);
     }
 
